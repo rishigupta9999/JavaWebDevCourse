8cfd1470283825f140218e1a6598c1fd65336b82
Rishi Gupta <rigupta@amazon.com>
2017-04-23 18:52:37 -0700

    Adding middleware

--- WebDevelopmentWithSpark/src/main/java/com/rishigupta/courses/Main.java
+++ WebDevelopmentWithSpark/src/main/java/com/rishigupta/courses/Main.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 
 import com.rishigupta.courses.model.SimpleCourseIdeaDAO;
 import com.rishigupta.courses.model.CourseIdeaDAO;
+import com.rishigupta.courses.model.CourseIdea;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -19,20 +20,53 @@ public class Main {
 
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
+        before ((req, res) -> {
+            if (req.cookie("username") != null)
+            {
+                req.attribute("username", req.cookie("username"));
+            }
+        });
+
+        before("/ideas", (req, res) -> {
+            if (req.attribute("username") == null) {
+                res.redirect("/");
+                halt();
+            }
+        });
+
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
-            model.put("username", req.cookie("username"));
+            model.put("username", req.attribute("username"));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/ideas", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            model.put("ideas", dao.findAll());
+
+            return new ModelAndView(model, "ideas.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) -> {
+            String title = req.queryParams("title");
+
+            CourseIdea idea = new CourseIdea(title, req.attribute("username"));
+            dao.add(idea);
+
+            res.redirect("/ideas");
+
+            return null;
+        });
 
         post("sign-in", (req, res) -> {
             Map<String, String> model = new HashMap<>();
             String username = req.queryParams("username");
             res.cookie("username", username);
 
-            model.put("username", username);
+            res.redirect("/");
 
-            return new ModelAndView(model, "sign-in.hbs");
-        }, new HandlebarsTemplateEngine());
+            return null;
+        });
     }
 }
